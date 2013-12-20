16c1349020e93dd83721d77cd27e6f338ceb6d17
AwesomeAbhay <abhayg47@gmail.com>
2013-12-19 19:16:17 -0500

    tested both directions

--- 2d_transverse.c
+++ 2d_transverse.c
@@ -52,7 +52,7 @@ double geth(int i, struct primitive * P){
 
 void initialize(struct vector * U, struct primitive * P, int i){
 	//Get x value, determine U based on x. All calculated from initial vars rho, p, v, and gam
-	double pos = getx(i);
+	double pos = gety(i);
 	if(pos<0.){
 		P[i].rho = 1.0;
 		P[i].v = 0.0;
@@ -64,8 +64,8 @@ void initialize(struct vector * U, struct primitive * P, int i){
 	}
 	else{
 		P[i].rho = 1.0;
-		P[i].v = 0.0;
-		P[i].vy = 0.99;
+		P[i].v = 0.99;
+		P[i].vy = 0.0;
 		P[i].W=getW(i,P);
 		P[i].p = 0.01;
 		P[i].eps = P[i].p/((gam-1.)*P[i].rho);
@@ -94,7 +94,7 @@ void updateFy(struct vector * U, struct vector * F, struct primitive * P){
 		F[i].mass = U[i].mass*P[i].vy;
 		F[i].mom = U[i].mom*P[i].vy;
 		F[i].momY = U[i].momY*P[i].vy+P[i].p;
-		F[i].ene = U[i].mom-U[i].mass*P[i].v;
+		F[i].ene = U[i].momY-U[i].mass*P[i].vy;
 	}
 }
 
@@ -135,7 +135,7 @@ struct vector getFluxX(int i, struct vector * U, struct vector * F, struct primi
 }
 
 double get_alphaY(int i, struct vector * U, struct vector * F, struct primitive * P, int plus){
-	double v = .5*(P[i].v+P[i+N].v);
+	double v = .5*(P[i].vy+P[i+N].vy);
 	double cs = .5*(getcs(i,P)+getcs(i+N,P));
 
 	double alpha;
@@ -259,8 +259,8 @@ void advanceTime(struct vector * U, struct vector * Fx, struct vector * Fy, stru
 	}
 	
 	updatePrimitive(U,P); 
-	fixBC(U,P);
-	//fixBCy(U,P);
+	//fixBC(U,P);
+	fixBCy(U,P);
 	
 	updateFx(U,Fx,P);
 	updateFy(U,Fy,P);
@@ -293,7 +293,8 @@ int main(){
 		while(currentTime<time){
 			advanceTime(U,Fx,Fy,P);
 			currentTime += dt;
-			if(isnan(P[250].rho)==1){
+			if(isnan(P[20000].rho)==1){
+			//if(P[100].rho!=P[100+N].rho){
 				printf("%f\n",currentTime);
 				break;
 			}
